e85cd2e482c1a9af342c72b22a5d2ee6557d76bf
paulpod <paulpod@neuromantics.net>
2019-07-10 13:11:46 +0100

    making a homepage with carousel prototype

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,39 @@ $(document).ready(function () {
 $(document).on('ready', function() {
 
   $('.single-item').slick({
-    dots: true,
+    
     initialSlide: 0,
-    arrows: true
+    arrows: true,
+    slidesToShow: 3,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 560,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
 
 });
