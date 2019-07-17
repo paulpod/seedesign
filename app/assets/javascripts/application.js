c709b25f7f0794f5acb881775fa50b56707f0ae4
paulpod <paulpod@neuromantics.net>
2019-07-17 09:55:52 +0100

    making progress on the carousels for new home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,38 @@ $(document).ready(function () {
 
 $(document).on('ready', function() {
 
-  $('.single-item').slick({
+  $('.about-slick').slick({
+    
+    initialSlide: 0,
+    arrows: true,
+    centerMode: false,
+    slidesToShow: 1,
+    responsive: [
+    
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 460,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+
+
+  $('.calc-slick').slick({
     
     initialSlide: 0,
     arrows: true,
