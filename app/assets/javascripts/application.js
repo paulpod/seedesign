f69594e6af42166690a02d15d7342be1303e7a2f
paulpod <paulpod@neuromantics.net>
2019-08-31 10:57:34 +0100

    messing with a flip panel for one-click investing on a campaign page ... might have broken form elements styles on homepage carousels!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,13 +11,99 @@ $(document).ready(function () {
 
 
 
+
+$(document).ready(function() {
+
+  var s_round = '.s_round';
+
+  var cancel_c50 = '.cancel_c50';
+  var cancel_other = '.cancel_other';
+
+  var c50_round = '.c50_oneclick';
+  var c_other = '.c_other';
+
+  $(s_round).hover(function() {
+    $('.b_round').toggleClass('b_round_hover');
+    return false;
+  });
+
+ 
+
+  
+  // flip the front and show the back for each button
+
+  $(c50_round).click(function() {
+    $('.flip_box').toggleClass('flipped');
+    $('#c50_back').toggleClass('display_none');
+    $('#c50_back').toggleClass('hidden_back');
+    //$(this).addClass('s_round_click');
+    $('.s_arrow').toggleClass('s_arrow_rotate');
+    $('.b_round').toggleClass('b_round_back_hover');
+    return false;
+  });
+
+  $(c_other).click(function() {
+    $('.flip_box').toggleClass('flipped');
+    $('#c_otherback').toggleClass('display_none');
+    $('#c_otherback').toggleClass('hidden_back');
+    //$(this).addClass('s_round_click');
+    $('.s_arrow').toggleClass('s_arrow_rotate');
+    $('.b_round').toggleClass('b_round_back_hover');
+    return false;
+  });
+
+
+  // now cancel, flip back, re-hide when moved
+
+  $(cancel_c50).click(function() {
+    $('.flip_box').toggleClass('flipped');
+    $('#c50_back').toggleClass('hidden_back');
+
+    $('#c50_back').one('transitionend', function() {
+      console.log('done 50');
+      $('#c50_back').toggleClass('display_none');
+    });
+
+    return false;
+  });
+
+  $(cancel_other).click(function() {
+    $('.flip_box').toggleClass('flipped');
+    $('#c_otherback').toggleClass('hidden_back');
+
+    $('#c_otherback').one('transitionend', function() {
+      console.log('done other');
+      $('#c_otherback').toggleClass('display_none');
+    });
+
+    return false;
+  });
+
+  
+
+
+
+
+
+  $(s_round).on('transitionend', function() {
+    $(this).removeClass('s_round_click');
+    $(this).addClass('s_round_back');
+    return false;
+  });
+});
+
+
+
+
+
+
 $(document).on('ready', function() {
 
   $('.about-slick').slick({
   dots: true,
   infinite: false,
   speed: 300,
-  slidesToShow: 2.2,
+  slidesToShow: 1,
   centerMode: false,
 
   variableWidth: true
@@ -26,14 +112,23 @@ $(document).on('ready', function() {
 
 
 
-    $('.calc-slick').slick({
+  $('.calc-slick').slick({
   dots: true,
   infinite: false,
   speed: 300,
-  slidesToShow: 2.2,
+  slidesToShow: 2,
   centerMode: false,
 
   variableWidth: true
+});
+
+
+  $('.hero-slick').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+
 });
 
 
